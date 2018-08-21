cb1d28cd7a771551fd7636af2259e07bba70bbb2
a.afonin <a.afonin@bars-open.ru>
2018-08-21 10:56:19 +0300

    сделано создание сетки.

--- server/core/room.go
+++ server/core/room.go
@@ -1,8 +1,11 @@
 package core
 
+// Временное решение для того что бы отличать id комнат
+var maxID int
+
 type Chunc struct {
 	State       int
-	Сoordinates [][]int
+	Сoordinates [][2]int
 }
 
 type Room struct {
@@ -10,3 +13,28 @@ type Room struct {
 	Map     []*Chunc
 	Clients []*Client
 }
+
+func (room *Room) createMap() {
+	var step = 100
+	var y = 0
+
+	for i := 0; i < 3; i++ {
+		var x = 0
+		for j := 0; j < 3; j++ {
+			chunc := Chunc{}
+			chunc.Сoordinates = append(
+				chunc.Сoordinates,
+				[2]int{x, y},
+				[2]int{x + step, y},
+				[2]int{x + step, y + step},
+				[2]int{x, y + step},
+			)
+
+			x += step
+
+			room.Map = append(room.Map, &chunc)
+		}
+	}
+}
+
+func () Start
